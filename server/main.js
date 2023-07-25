58d565e27d1b1891c58cedd0004891e560990cd6
Cohen <itamar2.cohen@intel.com>
2023-07-25 23:03:29 +0300

    adding dmessages

--- server/main.js
+++ server/main.js
@@ -12,11 +12,13 @@ app.get('/', (req, res) => res.send({data: "hello world!"}));
 // Routes
 const users = require('./routes/Users');
 const auth = require('./routes/user_auth');
-const contacts = require('./routes/contacts');
+const contacts = require('./routes/contacts');0
+const dmessages = require('./routes/dmessages');
 
 app.use('/', auth)
 app.use('/users', users);
 app.use('/contacts', contacts);
+app.use('/dmessages', dmessages);
 
 // Listen on port
 const port = process.env.PORT || 3000;
