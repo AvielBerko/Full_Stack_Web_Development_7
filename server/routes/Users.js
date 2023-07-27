ef2688d9fbb024faa1187972ba48ded39eba2bb6
Cohen <itamar2.cohen@intel.com>
2023-07-27 18:23:52 +0300

    allow space

--- server/routes/Users.js
+++ server/routes/Users.js
@@ -5,7 +5,7 @@ const Joi = require('joi');
 
 const updated_user_schema = Joi.object({
   id: Joi.string().guid({ version: ['uuidv4']}).when('new', {is: true, then: Joi.required()}),
-  username: Joi.string().min(3).max(30).alphanum(),
+  username: Joi.string().min(3).max(30).alphanum().allow(' '),
   phone_number: Joi.string().min(10).max(15)
 })
 
