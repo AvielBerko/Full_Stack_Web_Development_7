1bf93ec736d9156fa7351c9c035188bf85ab7eee
Aviel Berkowitz <9155988@gmail.com>
2023-07-30 14:09:37 +0300

    jwt start

--- client/chatapp/src/api/register.js
+++ client/chatapp/src/api/register.js
@@ -2,29 +2,19 @@ import { add } from "./axios_api.js"
 import routes from "../env.js";
 
 export default function register(user)  {
-  return add(routes.register, user);
-  // return axios
-  // .post(routes.register, user)
-  // .then((response) => {
-  //   if (response.status === 200) {
-  //     return response.data;
-  //   } else {
-  //     return null;
-  //   }
-  // }).catch((err) => {
-  //   if (err.response.data) {
-  //     throw new Error(err.response.data.error);
-  //   }
-  //   throw new Error (err.message);
-  // });
-  // .then((data) => {
-  //   if (!data.ok) {
-  //     return data;
-  //   }
-  //   return {
-  //     ok: true,
-  //     user: data.user
-  //     //user: { ...jwtDecode(data.token), token: data.token },
-  //   };
-  // });
+  // return add(routes.register, user);
+  return axios
+  .post(routes.register, user)
+  .then((response) => {
+      return response.data;
+  })
+  .then((token) => {
+    return { ...jwtDecode(token), token };
+  })
+  .catch((err) => {
+    if (err.response.data.error) {
+      throw new Error(err.response.data.error);
+    }
+    throw new Error(err.message);
+  });
 }
