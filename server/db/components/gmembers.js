c6ba39c64ba9fe0cfb763099e10b25f6cf6a755c
Cohen <itamar2.cohen@intel.com>
2023-07-30 14:08:54 +0300

    email to member and start and length to read

--- server/db/components/gmembers.js
+++ server/db/components/gmembers.js
@@ -3,7 +3,25 @@ const generic = require('../crud.js');
 const tables = require('../table_names.js');
 
 async function getGroupMembers(group_id){
-    return generic.read(tables.GROUP_MEMBERS, {groupchat_id: group_id, valid: true});
+    //return generic.read(tables.GROUP_MEMBERS, {groupchat_id: group_id, valid: true});
+    return new Promise((resolve, reject) => {
+        db_connection.getConnection(con => {
+            con.query(`
+                SELECT ${tables.GROUP_MEMBERS}.*, email FROM
+                ${tables.GROUP_MEMBERS}
+                INNER JOIN
+                ${tables.USERS}
+                ON ${tables.GROUP_MEMBERS}.user_id = ${tables.USERS}.id
+                WHERE groupchat_id = ? AND ${tables.USERS}.valid = 1 AND ${tables.GROUP_MEMBERS}.valid = 1
+                ORDER BY ${tables.GROUP_MEMBERS}.id
+                ;`,
+                [group_id],
+            (error, result) => {
+                if (error) reject(error);
+                resolve(result);
+            })
+        });
+    });
 }
 
 async function addGroupMember(new_gmember){
