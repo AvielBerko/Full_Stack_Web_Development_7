74ce15c0487dae4f30271859e164d40641be512e
Cohen <itamar2.cohen@intel.com>
2023-08-01 15:57:53 +0300

    fix bug

--- server/db/components/groups.js
+++ server/db/components/groups.js
@@ -40,9 +40,9 @@ async function updateGroup(updated_group, user_id) {
             ${tables.GROUP_MEMBERS}
             ON ${tables.GROUP_MEMBERS}.groupchat_id = ${tables.GROUPS}.id
             SET ${tables.GROUPS}.name = ?
-            WHERE user_id = ? AND ${tables.GROUP_MEMBERS}.admin = 1 AND ${tables.GROUP_MEMBERS}.valid = 1 AND ${tables.GROUPS}.valid = 1
+            WHERE user_id = ? AND ${tables.GROUPS}.id = ? AND ${tables.GROUP_MEMBERS}.admin = 1 AND ${tables.GROUP_MEMBERS}.valid = 1 AND ${tables.GROUPS}.valid = 1
             ;`,
-                [updated_group.name, user_id],
+                [updated_group.name, user_id, updated_group.id],
                 (error, result) => {
                     if (error) reject(error);
                     resolve(result);
